66080fe33b3a80052fac6dfe65124075207e79fd
Michael Benedict <michaelbt@live.com>
2021-01-29 18:12:30 +0700

    sm8150-common: Don't exclude lineage overlays from RRO

--- common.mk
+++ common.mk
@@ -20,9 +20,9 @@ PRODUCT_PACKAGE_OVERLAYS += \
     $(COMMON_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(COMMON_PATH)/overlay-lineage/lineage-sdk \
-    $(COMMON_PATH)/overlay-lineage/packages/apps/Snap
+# PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+#     $(COMMON_PATH)/overlay-lineage/lineage-sdk \
+#     $(COMMON_PATH)/overlay-lineage/packages/apps/Snap
 
 # Audio 
 PRODUCT_PACKAGES += \

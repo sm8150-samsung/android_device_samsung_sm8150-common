e66feb208331321749cec0e6f2a9b1787d3f6505
PizzaG <49663808+PizzaG@users.noreply.github.com>
2021-04-28 02:22:23 -0500

    Remove Uneeded Custom APN, revert/fix arch

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -18,9 +18,6 @@ ifeq ($(PRODUCT_DEVICE), d2q)
 	TARGET_FLATTEN_APEX := true
 endif
 
-# APN (Custom APN)
-CUSTOM_APNS_FILE := $(COMMON_PATH)/configs/apns-full-conf.xml
-
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a
@@ -30,11 +27,11 @@ TARGET_CPU_VARIANT := cortex-a76
 TARGET_CPU_VARIANT_RUNTIME := cortex-a76
 
 TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_ARCH_VARIANT := armv8-a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a9
-TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a9
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a76
 
 TARGET_USES_64_BIT_BINDER := true
 ENABLE_CPUSETS := true

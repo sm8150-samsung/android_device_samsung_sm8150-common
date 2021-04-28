475fb9fa576c32a277252a21a003056ba2a77352
PizzaG <49663808+PizzaG@users.noreply.github.com>
2021-04-27 22:20:52 -0500

    Add files via upload

--- Android.mk
+++ Android.mk
@@ -1,9 +1,3 @@
-#
-# Copyright (C) 2020 The LineageOS Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter r5q r5que beyond0qlte beyond1qlte beyond2qlte d1q d2q,$(TARGET_DEVICE)),)

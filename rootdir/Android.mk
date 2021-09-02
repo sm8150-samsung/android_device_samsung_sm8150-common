d6d8b3881aa82029be8ccd3551156d9ed90f8d7b
Michael Benedict <michaelbt@live.com>
2021-09-02 15:12:47 +0700

    sm8150-common: Create power init script

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -13,3 +13,10 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.power.qcom.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.power.qcom.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
+include $(BUILD_PREBUILT)
